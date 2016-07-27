55194dc87d31b219540f30a22cfcd74f9a5b7b35
Lóránt Pintér <lorant@gradle.com>
2016-07-27 19:45:24 +0200

    Make the plugin Java 1.7 compatible

--- src/main/java/org/gradle/cache/tasks/hazelcast/HazelcastPlugin.java
+++ src/main/java/org/gradle/cache/tasks/hazelcast/HazelcastPlugin.java
@@ -5,7 +5,9 @@ import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.core.HazelcastInstance;
 import org.gradle.StartParameter;
 import org.gradle.api.Plugin;
-import org.gradle.api.internal.tasks.cache.*;
+import org.gradle.api.internal.tasks.cache.MapBasedTaskOutputCache;
+import org.gradle.api.internal.tasks.cache.TaskOutputCache;
+import org.gradle.api.internal.tasks.cache.TaskOutputCacheFactory;
 import org.gradle.api.invocation.Gradle;
 
 public class HazelcastPlugin implements Plugin<Gradle> {
@@ -15,17 +17,22 @@ public class HazelcastPlugin implements Plugin<Gradle> {
             @Override
             public TaskOutputCache createCache(StartParameter startParameter) {
                 ClientConfig config = new ClientConfig();
-                String host = startParameter.getSystemPropertiesArgs().getOrDefault("org.gradle.cache.tasks.hazelcast.host", "127.0.0.1");
-                String port = startParameter.getSystemPropertiesArgs().getOrDefault("org.gradle.cache.tasks.hazelcast.port", "5701");
-                String name = startParameter.getSystemPropertiesArgs().getOrDefault("org.gradle.cache.tasks.hazelcast.name", "gradle-task-cache");
+                String host = getSystemProperty(startParameter, "org.gradle.cache.tasks.hazelcast.host", "127.0.0.1");
+                String port = getSystemProperty(startParameter, "org.gradle.cache.tasks.hazelcast.port", "5701");
+                String name = getSystemProperty(startParameter, "org.gradle.cache.tasks.hazelcast.name", "gradle-task-cache");
                 String address = host + ":" + port;
                 config.getNetworkConfig().addAddress(address);
                 final HazelcastInstance instance = HazelcastClient.newHazelcastClient(config);
                 return new MapBasedTaskOutputCache(
                     String.format("Hazelcast cache '%s' at %s", name, address),
-                    instance.getMap(name)
+                    instance.<String, byte[]>getMap(name)
                 );
             }
         });
+    }
+
+    private static String getSystemProperty(StartParameter startParameter, String property, String defaultValue) {
+        String value = startParameter.getSystemPropertiesArgs().get(property);
+        return value == null || value.isEmpty() ? defaultValue : value;
     }
 }
